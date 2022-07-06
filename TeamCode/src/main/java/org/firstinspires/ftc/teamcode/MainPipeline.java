a8243e4d3858714d594f1ea80dc7c420b9420654
rikhilkumar <rikhiljkumar@gmial.com>
2022-07-06 10:40:59 -0400

    changed channel numbers to correspond correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainPipeline.java
@@ -30,15 +30,15 @@ public class MainPipeline extends OpenCvPipeline {
         int x1[] = {0, 0, 0};
         int y1[] = {0, 0, 0};
         int x2[] = {0, 0, 0};
-        int y2[] = {0, 0, 0};
+        int y2[] = {0, 0, 0};//can use 2d arrays
 
         Mat YCrCb = new Mat();
         Mat Cr = new Mat();
 
         Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(YCrCb, Cr, 2);
+        Core.extractChannel(YCrCb, Cr, 1);
 
-        Mat region1_Cb, region2_Cb, region3_Cb;//3 regions to test for RED shipping element
+        Mat region1_Cr, region2_Cr, region3_Cr;//3 regions to test for RED shipping element
         Point region1_pointA = new Point(
                 x1[0],
                 y1[0]);
@@ -58,19 +58,19 @@ public class MainPipeline extends OpenCvPipeline {
                 x2[2],
                 y2[2]);
 
-        region1_Cb = Cr.submat(new Rect(region1_pointA, region1_pointB));
-        region2_Cb = Cr.submat(new Rect(region2_pointA, region2_pointB));
-        region3_Cb = Cr.submat(new Rect(region3_pointA, region3_pointB));
+        region1_Cr = Cr.submat(new Rect(region1_pointA, region1_pointB));
+        region2_Cr = Cr.submat(new Rect(region2_pointA, region2_pointB));
+        region3_Cr = Cr.submat(new Rect(region3_pointA, region3_pointB));
         /* (x1,y1) and (x2,y2) are 2 points in a coordinate plane (in this case with the units being pixels)
         they bound the areas the program looks at
         i made an array of these coordinates
         also this is done in the YCrCb color code cuz that is better cuz its not brightness dependent unlike RGB
-        YCbCr looks at blueness and redness (and brightness)
+        YCbCr looks at blueness and redness (and brightness, but you can ignore brightness because that is a seperate variable)
         https://tvone.com/tech-support/faqs/120-ycrcb-values-for-various-colors godsend
          */
-        avg_redLeft = (int) Core.mean(region1_Cb).val[0];
-        avg_redMiddle = (int) Core.mean(region2_Cb).val[0];
-        avg_redRight = (int) Core.mean(region3_Cb).val[0];
+        avg_redLeft = (int) Core.mean(region1_Cr).val[0];
+        avg_redMiddle = (int) Core.mean(region2_Cr).val[0];
+        avg_redRight = (int) Core.mean(region3_Cr).val[0];
 
 
         //cosmetic boxes around areas
@@ -93,10 +93,9 @@ public class MainPipeline extends OpenCvPipeline {
                 RED, // The color the rectangle is drawn in
                 2); // Thickness of the rectangle lines
 
-        int minOneTwo = Math.min(avg_redLeft, avg_redMiddle);
-        int min = Math.min(minOneTwo, avg_redRight);
+        int max = Math.max(Math.max(avg_redLeft, avg_redMiddle), avg_redRight);
 
-        if (min == avg_redLeft) {
+        if (max == avg_redLeft) {
             position = ShippingElementPosition.LEFT;
             Imgproc.rectangle(
                     input, // Buffer to draw on
@@ -105,7 +104,7 @@ public class MainPipeline extends OpenCvPipeline {
                     GREEN, // The color the rectangle is drawn in
                     -1); // Negative thickness means solid fill
 
-        } else if (min == avg_redMiddle) {
+        } else if (max == avg_redMiddle) {
             position = ShippingElementPosition.CENTER;
             Imgproc.rectangle(
                     input, // Buffer to draw on
@@ -113,7 +112,7 @@ public class MainPipeline extends OpenCvPipeline {
                     region2_pointB, // Second point which defines the rectangle
                     GREEN, // The color the rectangle is drawn in
                     -1); // Negative thickness means solid fill
-        } else if (min == avg_redRight) {
+        } else if (max == avg_redRight) {
             position = ShippingElementPosition.RIGHT;
             Imgproc.rectangle(
                     input, // Buffer to draw on
